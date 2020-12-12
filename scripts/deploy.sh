e991cf90cdc2fdc678365fc8e0869e1cdd230aa7
whattheearl <earl.jonathan@gmail.com>
2020-12-11 17:10:45 -0800

    add shebang, use absolute path

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,15 @@
-echo removing project
-sudo rm -rf ~/express
+#!/bin/bash
 
-echo "cloning https://github.com/whattheearl/reverse-proxy.git to ~/express"
-git clone https://github.com/whattheearl/reverse-proxy.git ~/express
+EC2HOME="/home/ec2-user"
+
+echo removing project
+sudo rm -rf "${EC2HOME}/express"
+
+echo "cloning https://github.com/whattheearl/reverse-proxy.git to ${EC2HOME}/express"
+sudo git clone https://github.com/whattheearl/reverse-proxy.git "${EC2HOME}/express"
 
 echo remove services
-cd ~/express
+cd "${EC2HOME}/express"
 docker-compose down
 
 echo spinning up services
