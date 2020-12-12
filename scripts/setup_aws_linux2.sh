03bc8772d29f7b60776a4e287e6d9820699005dd
whattheearl <earl.jonathan@gmail.com>
2020-12-11 16:23:55 -0800

    echo to log file

--- scripts/setup_aws_linux2.sh
+++ scripts/setup_aws_linux2.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
-echo  installing docker
+echo  installing docker >> /logs/setup.txt
 sudo amazon-linux-extras install docker
 
-echo start docker
+echo start docker >> /logs/setup.txt
 sudo service docker start
 
-echo add ec2-user to docker group
+echo add ec2-user to docker group >> /logs/setup.txt
 sudo usermod -a -G docker ec2-user
 
-echo auto start docker
+echo auto start docker >> /logs/setup.txt
 sudo chkconfig docker on
 
-echo installing git
+echo installing git >> /logs/setup.txt
 sudo yum install -y git
 
 # reboot?
-echo installing docker-compose
+echo installing docker-compose >> /logs/setup.txt
 sudo curl -L  https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
+echo "adding crontab for deploy script" >> /logs/setup.txt
 (crontab -l 2>/dev/null; echo "@reboot /express/deploy.sh") | crontab -
 
 sudo reboot
