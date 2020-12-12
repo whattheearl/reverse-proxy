f687ebe9f8a80b9cba8b772df9e211113ac45e71
whattheearl <earl.jonathan@gmail.com>
2020-12-11 17:18:54 -0800

    create as user

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,7 +6,7 @@ echo removing project
 sudo rm -rf "${EC2HOME}/express"
 
 echo "cloning https://github.com/whattheearl/reverse-proxy.git to ${EC2HOME}/express"
-sudo git clone https://github.com/whattheearl/reverse-proxy.git "${EC2HOME}/express"
+git clone https://github.com/whattheearl/reverse-proxy.git "${EC2HOME}/express"
 
 echo remove services
 cd "${EC2HOME}/express"
